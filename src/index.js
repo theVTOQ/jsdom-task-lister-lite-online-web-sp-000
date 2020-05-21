a1f4d6e0639b75b03eeef901aed78d29baf1a3d7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-21 19:13:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const todoList = document.getElementByTagName("ul");
   const newTaskListForm = document.getElementById("create-new-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
-  const taskNameArray = [];
+  const taskDescriptionArray = [];
 
   newTaskListForm.addEventListener("submit", function (event){
     event.preventDefault();
-    taskNameArray.push(newTaskDescription.value)
+    taskDescriptionArray.push(newTaskDescription.value)
     refreshTodoList();
     //reset form:
     event.target.reset();
   });
 
   todoList.addEventListener("click", function (event){
-    if (event.target.nodeName === "BUTTON"){
-      taskNameArray = taskNameArray.filter((value) => value !=== event.target.dataset.description)
+    if (event.target.nodeDescription === "BUTTON"){
+      taskDescriptionArray = taskDescriptionArray.filter((value) => value !=== event.target.dataset.description)
     }
     refreshTodoList();
   });
 
   refreshTodoList() {
     let innerHtmlForList = "";
-    for (let i = 0; i < taskNameArray.length; i++) {
-      innerHtml += `<li>${taskNameArray[i]} <button data-description="${taskNameArray[i]}">X</button></li>`;
+    for (let i = 0; i < taskDescriptionArray.length; i++) {
+      innerHtml += `<li>${taskDescriptionArray[i]} <button data-description="${taskDescriptionArray[i]}">X</button></li>`;
     }
     todoList.innerHtml = innerHtmlForList;
   }
