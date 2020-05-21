73f36a7dfecaad345be1bc5f0e50169051c982e4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-21 18:23:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const newTaskListForm = document.getElementById("create-new-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
 
-  submitButton.addEventListener("submit", function (event){
+  newTaskListForm.addEventListener("submit", function (event){
     event.preventDefault();
     const newListButton = document.createElement("li");
     newListButton.innerHtml = newTaskDescription.value;
