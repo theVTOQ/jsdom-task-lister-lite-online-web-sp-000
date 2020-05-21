b51f750fe39e0b95f008cdf819594bd2637987b4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-21 17:36:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const todoList = document.getElementByTagName("ul");
+  const submitButton = document.getElementByTagName("sumbit");
+
+  submitButton.addEventListener("submit", function (event){
+    event.preventDefault();
+    const newListButton = document.createElement("li");
+    newListButton.innerHtml = event.text;
+    todoList.appendChild(newListButton)
+  });
 });
