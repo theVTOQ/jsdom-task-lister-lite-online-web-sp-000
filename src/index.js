9dfa26e10bd2f616f2f019f9caf73e30d45793d1
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-21 18:14:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const todoList = document.getElementByTagName("ul");
   const newTaskListForm = document.getElementById("create-new-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
   const submitButton = document.getElementByTagName("sumbit");
 
   submitButton.addEventListener("submit", function (event){
