7bf607e13d8e96984754cb34f8a27eece9e80a2e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-21 18:34:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const todoList = document.getElementByTagName("ul");
   const newTaskListForm = document.getElementById("create-new-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
+  const taskNameArray = [];
 
   newTaskListForm.addEventListener("submit", function (event){
     event.preventDefault();
     const newTask = document.createElement("li");
-    newTask.innerHtml = `${newTaskDescription.value} <button data-description="${this.description}">X</button>`;
-    todoList.appendChild(newTask);
+    taskNameArray.push(newTaskDescription.value)
+    
     //reset form:
     event.target.reset();
   });
 
+  todoList.addEventListener("click", function (event){
+    if (event.target.nodeName === "BUTTON"){
+      
+    }
+  });
 
+  refreshTodoList() {
+    for (let i = 0; i < todoList.length(); i++) {
+      newTask.innerHtml = `${newTaskDescription.value} <button data-description="${this.description}">X</button>`;
+      todoList.appendChild(newTask);
+    }
+  }
 });
